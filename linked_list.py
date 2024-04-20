2566937a1fc89628355b300126d1b62a9cba71f1
SuperMoonChild <167145755+SuperMoonChild@users.noreply.github.com>
2024-04-20 12:10:23 -0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -19,3 +19,32 @@ class Solution:
             prev = curr 
             curr = next 
         return prev 
+
+#Merge Two Linked List 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode() 
+        tail = dummy 
+
+        while list1 and list2: 
+            if list1.val < list2.val: 
+                tail.next = list1  
+                list1 = list1.next 
+            else: 
+                tail.next =list2
+                list2 = list2.next 
+            tail = tail.next 
+
+        #fill in the rest of the linkedlist 
+        if list1: 
+            tail.next = list1 
+        if list2: 
+            tail.next = list2 
+        
+        return dummy.next 
+        
